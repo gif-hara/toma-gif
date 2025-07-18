ae4e4887c8542d4c76a2c7dd1a90e5c376fff121
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-18 21:50:18 +0900

    MainSceneControllerクラスに敵生成機能を追加

--- toma-gif/Assets/toma-gif/Scripts/MainSceneController.cs
+++ toma-gif/Assets/toma-gif/Scripts/MainSceneController.cs
@@ -1,8 +1,28 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace tomagif
 {
     public class MainSceneController : MonoBehaviour
     {
+        [field: SerializeField]
+        private Actor player;
+
+        [field: SerializeField]
+        private Actor enemyPrefab;
+
+        [field: SerializeField]
+        private Transform enemySpawnPointParent;
+
+        private readonly List<Actor> enemies = new();
+
+        void Start()
+        {
+            foreach (Transform spawnPoint in enemySpawnPointParent)
+            {
+                var enemyActor = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+                enemies.Add(enemyActor);
+            }
+        }
     }
 }
