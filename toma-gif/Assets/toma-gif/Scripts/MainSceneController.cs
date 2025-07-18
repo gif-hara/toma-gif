d9eaa54334f133fac392c6760eac450b1a8cd91e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-18 22:33:37 +0900

    MainSceneControllerにUIViewInGameの初期化とボタンクリックの監視機能を追加

--- toma-gif/Assets/toma-gif/Scripts/MainSceneController.cs
+++ toma-gif/Assets/toma-gif/Scripts/MainSceneController.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
 using UnityEngine;
 
 namespace tomagif
@@ -14,7 +17,12 @@ namespace tomagif
         [field: SerializeField]
         private Transform enemySpawnPointParent;
 
+        [field: SerializeField]
+        private HKUIDocument inGameDocument;
+
         private readonly List<Actor> enemies = new();
+
+        private UIViewInGame uiViewInGame;
 
         void Start()
         {
@@ -22,6 +30,28 @@ namespace tomagif
             {
                 var enemyActor = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
                 enemies.Add(enemyActor);
+            }
+            uiViewInGame = new UIViewInGame(inGameDocument);
+            uiViewInGame.Initialize();
+            uiViewInGame.Activate();
+            BeginObserveJudgementButtonAsync(CancellationToken.None).Forget();
+        }
+
+        private async UniTask BeginObserveJudgementButtonAsync(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var isTrue = await uiViewInGame.OnClickJudgementButtonAsync(cancellationToken);
+                if (isTrue)
+                {
+                    // Handle true button click
+                    Debug.Log("True button clicked");
+                }
+                else
+                {
+                    // Handle false button click
+                    Debug.Log("False button clicked");
+                }
             }
         }
     }
