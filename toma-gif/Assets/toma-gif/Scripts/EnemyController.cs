d9a7a707011a9cbfd403c9b11d4f154f4fffad2d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-07-19 11:19:47 +0900

    EnemyControllerクラスを新規作成し、移動およびアニメーション再生メソッドを追加

---
+++ toma-gif/Assets/toma-gif/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+using Cysharp.Threading.Tasks;
+using HK;
+using LitMotion;
+using LitMotion.Extensions;
+using UnityEngine;
+
+namespace tomagif
+{
+    public class EnemyController
+    {
+        private readonly HKUIDocument document;
+
+        private readonly Animator animator;
+
+        public GameObject RootGameObject => document.gameObject;
+
+        public EnemyController(HKUIDocument document)
+        {
+            this.document = document;
+            this.animator = document.Q<Animator>("Animator");
+        }
+
+        public UniTask MoveAsync(Vector3 toPosition, float enemyMoveDuration, Ease enemyMoveEase)
+        {
+            return LMotion.Create(document.transform.position, toPosition, enemyMoveDuration)
+                .WithEase(enemyMoveEase)
+                .BindToPosition(document.transform)
+                .AddTo(document.transform)
+                .ToUniTask();
+        }
+
+        public void PlayIdleAnimation()
+        {
+            animator.Play("Idle");
+        }
+
+        public void PlayDeadAnimation()
+        {
+            animator.Play("Dead");
+        }
+    }
+}
